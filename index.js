13266c20b2be05b6b42f3f7fab906a3ecee06518
davidmodfyi <david@mod.fyi>
2025-05-08 22:19:56 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname);
+    cb(null, 'logo-' + req.session.distributor_id + '-' + uniqueSuffix + ext);
+  }
+});
+
 const upload = multer({ 
   storage: storage,
   limits: {
@@ -43,18 +57,6 @@ const upload = multer({
       return cb(new Error('Only image files are allowed!'), false);
     }
     cb(null, true);
-  }
-});
-
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, 'logo-' + req.session.distributor_id + '-' + uniqueSuffix + ext);
   }
 });
 
